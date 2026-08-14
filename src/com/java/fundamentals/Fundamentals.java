eee234eae3f9f4cd8952df78318c56cb32fe9835
chitrabollapalli <chitrabollapalli@gmail.com>
2026-08-14 16:29:40 +0530

    additionNumber

--- src/com/java/fundamentals/Fundamentals.java
+++ src/com/java/fundamentals/Fundamentals.java
@@ -3,5 +3,6 @@ package com.java.fundamentals;
 public class Fundamentals {
     public static void main(String[] args) {
         System.out.println("My first java program");
+        System.out.println("changing file");
     }
 }
---
+++ src/com/java/logicalPrograms/AdditionNumber.java
@@ -0,0 +1,9 @@
+package com.java.logicalPrograms;
+
+public class AdditionNumber {
+    public static void main(String[] args) {
+        int i=10;
+        int j=20;
+         System.out.println(i+j);
+    }
+}
